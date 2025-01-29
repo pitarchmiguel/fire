2930035e17fef01830042daeba37ea85b3bab267
pitarchmiguel <pitarchmiguel@gmail.com>
2025-01-29 17:23:22 +0100

    updated layout

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}',
+  ],
+  safelist: [
+    'bottom-0',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
