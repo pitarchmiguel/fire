b03a554faf925d95761d4d1fa756a8fd3f92afaa
pitarchmiguel <pitarchmiguel@gmail.com>
2025-01-29 18:02:19 +0100

    updated layout

--- public/main.js
+++ public/main.js
@@ -5,11 +5,11 @@ const cerrar = document.querySelector('#cerrar-popup');
     button.addEventListener('click', () => {
         console.log('click');
         const popup = document.querySelector('.popup');
-        popup.classList.toggle('hidden');
+        popup.classList.toggle('bottom-0');
     });
 
     cerrar.addEventListener('click', () => {
         console.log('cerrar');
         const popup = document.querySelector('.popup');
-        popup.classList.toggle('hidden');
+        popup.classList.toggle('bottom-0');
     });
